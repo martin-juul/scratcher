bd994739dc400b6a0686b03062fca365d16a516e
Martin Juul <code@juul.xyz>
2021-03-14 13:08:16 +0100

    check if file exists or if it's readable before trying to serve it

--- server/app/Http/Controllers/StreamController.php
+++ server/app/Http/Controllers/StreamController.php
@@ -10,6 +10,14 @@ class StreamController extends Controller
 {
     public function file(Request $request, Track $track)
     {
+        if (!file_exists($track->path)) {
+            return response()->json(['error' => 'file_not_found'], 500);
+        }
+
+        if (!is_readable($track->path)) {
+            return response()->json(['error' => 'file_not_readable'], 500);
+        }
+
         $stream = static function () use ($track) {
             $source = fopen($track->path, 'rb');
             $dest = fopen('php://output', 'wb');
